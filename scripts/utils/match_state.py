185b5f99ce2bd1c09e081b946e3893e30dfcf3c1
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2021-08-27 15:20:17 -0300

    ✨ Add start lib

---
+++ scripts/utils/match_state.py
@@ -0,0 +1,34 @@
+import rospy
+from std_msgs.msg import Bool
+
+class MatchState:
+    def __init__(self, topic_name):
+        """Cria um novo objeto para fazer a leitura do estado da partida
+
+        Args:
+            topic_name (string): Nome do tópico do estado da partida
+        """
+        self.state = False
+        self.topic_name = topic_name
+
+    def initialise(self):
+        """Inicializa o subscriber para leitura do estado da partida
+        """
+        rospy.Subscriber(self.topic_name, Bool, self._callback)
+        rospy.loginfo(f"Inicializando leitor do estado da partida {rospy.get_time()}")
+
+    def _callback(self, data):
+        """Função de callback necessária para receber informação do subscriber
+
+        Args:
+            data (std_msgs/UInt32): Dados da leitura do estado da partida
+        """
+        self.brightness = data.data
+
+    def started(self):
+        """ Método para obter o estado da partida
+            O estado será verdeiro se já tiver começado e falso caso contrário
+        Returns:
+            Bool : retorna o estado da partida
+        """
+        return self.state
