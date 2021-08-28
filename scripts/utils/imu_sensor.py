ba39b042193f7fa5be360bd675c32a64fb10d7c6
mariarezende07 <maloasan@gmail.com>
2021-08-28 19:57:45 -0300

    ✨ Add linear acceleration lib

--- scripts/utils/imu_sensor.py
+++ scripts/utils/imu_sensor.py
@@ -9,6 +9,7 @@ class ImuSensor:
             topic_name (string): Nome do tópico da Imu
         """
         self.angular_velocity = 0
+        self.linear_acceleration = 0
         self.topic_name = topic_name
 
     def initialise(self):
@@ -24,6 +25,7 @@ class ImuSensor:
             data (sensor_msgs.msg.Imu): Dados da leitura da Imu
         """
         self.angular_velocity = data.angular_velocity
+        self.linear_acceleration = data.linear_acceleration
 
     def get_angular_velocity(self):
         """Método para obter o valor da variação da velocidade angular
@@ -32,3 +34,11 @@ class ImuSensor:
             float: Variação da velocidade angular nos 3 eixos
         """
         return self.angular_velocity
+
+    def get_linear_acceleration(self):
+        """Método para obter o valor da variação da aceleração linear
+
+        Returns:
+            float: Variação da aceleração linear
+        """
+        return self.linear_acceleration
